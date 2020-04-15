7d69797017899106a22cb35ca79d6b400c8dfe95
smiddyxd <ascqwd123@gmail.com>
2020-04-15 16:17:27 +0200

    worked on frontend and added mysql connection

--- server.js
+++ server.js
@@ -24,3 +24,44 @@ app.use('/api/items', items)
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log('server started on ' + port))
+
+
+// mysql
+
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "asdqwe123",
+//     database: "mydb"
+// });
+
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected to mysql");
+//     let sql = ''
+//     // sql = "CREATE DATABASE mydb"
+//     sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))"
+//     con.query(sql, function (err, result) {
+//         if (err) throw err;
+//         console.log("Database created");
+//     });
+// });
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'asdqwe123',
+    database: 'mydb'
+});
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('connected')
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
